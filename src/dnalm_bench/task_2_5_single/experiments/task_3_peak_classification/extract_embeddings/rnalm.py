367ac83d4c97ea83783a9f944126c04dd8c272fd
PanosAntoniadis <pantoniadis97@gmail.com>
2025-10-15 09:37:57 +0200

    Change paths in rnalm task 3

--- src/dnalm_bench/task_2_5_single/experiments/task_3_peak_classification/extract_embeddings/rnalm.py
+++ src/dnalm_bench/task_2_5_single/experiments/task_3_peak_classification/extract_embeddings/rnalm.py
@@ -9,14 +9,14 @@ root_output_dir = os.environ.get("DART_WORK_DIR", "")
 
 if __name__ == "__main__":
     model_name = "rnalm"
-    output_dir = "/tmp/vqj407/rnalm_erda/gefion_output/outputs/mlm_track_metadata/runs/2025-07-15_11-11-12_144M_fukushima_w_taxonomy_no_tax_loss_resume"
+    output_dir = "/tmp/vqj407/rnalm_erda/gefion_output/outputs/mlm_track_metadata/runs/2025-06-22_16-53-44_144M"
     checkpoint_path = "best"
     use_metadata = False
     tokenizer_path = "/home/vqj407/workspace/RNALM/rnalm/tokenizers/dna_tokenizer"
     genome_fa = os.path.join(root_output_dir,"refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta")
     elements_tsv = os.path.join(root_output_dir,"task_3_peak_classification/processed_inputs/peaks_by_cell_label_unique_dataloader_format.tsv")
     chroms = None
-    batch_size = 64
+    batch_size = 512
     num_workers = 0
     seed = 0
     device = "cuda"
