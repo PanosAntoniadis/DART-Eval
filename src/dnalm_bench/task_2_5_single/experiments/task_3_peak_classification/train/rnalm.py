b4e27d4823be325babb0ecc86ceba5c4ff1eccc4
PanosAntoniadis <pantoniadis97@gmail.com>
2025-10-16 21:52:29 +0300

    add rnalm trainer

---
+++ src/dnalm_bench/task_2_5_single/experiments/task_3_peak_classification/train/rnalm.py
@@ -0,0 +1,100 @@
+import os
+import sys
+import wandb
+
+from torch.utils.data import DataLoader
+
+from ....training import PeaksEmbeddingsDataset, CNNEmbeddingsPredictor, train_peak_classifier
+
+root_output_dir = os.environ.get("DART_WORK_DIR", "")
+
+if __name__ == "__main__":
+    resume_checkpoint = int(sys.argv[1]) if len(sys.argv) > 1 else None
+
+    model_name = "rnalm"
+    peaks_h5 = os.path.join(root_output_dir, f"task_3_peak_classification/embeddings/{model_name}.h5")
+    elements_tsv = os.path.join(root_output_dir, "task_3_peak_classification/processed_inputs/peaks_by_cell_label_unique_dataloader_format.tsv")
+
+    batch_size = 1024
+    num_workers = 0
+    prefetch_factor = None
+    seed = 0
+    device = "cuda"
+
+    wandb.init(
+        project="dart_eval_task3",
+        name="train_probing_classification",
+        entity="RNALM",
+        dir="outputs/wandb",
+        config={
+            "model_name": model_name,
+            "task": "task_3",
+            "approach": "train_probing",
+            "batch_size": batch_size,
+            "num_workers": num_workers,
+            "prefetch_factor": prefetch_factor,
+            "seed": seed,
+            "device": device,
+        }
+    )
+    
+    chroms_train = [
+        "chr1",
+        "chr2",
+        "chr3",
+        "chr4",
+        "chr7",
+        "chr8",
+        "chr9",
+        "chr11",
+        "chr12",
+        "chr13",
+        "chr15",
+        "chr16",
+        "chr17",
+        "chr19",
+        "chrX",
+        "chrY"
+    ]
+    
+    chroms_val = [
+        "chr6",
+        "chr21"
+    ]
+
+    chroms_test = [
+        "chr5",
+        "chr10",
+        "chr14",
+        "chr18",
+        "chr20",
+        "chr22"
+    ]
+
+    input_channels = 1024
+    hidden_channels = 32
+    kernel_size = 8
+
+    crop = 557
+
+    lr = 2e-3
+    num_epochs = 150
+
+    out_dir = os.path.join(root_output_dir, f"task_3_peak_classification/supervised_models/probed/{model_name}")   
+    os.makedirs(out_dir, exist_ok=True)
+
+    classes = {
+        "GM12878": 0,
+        "H1ESC": 1,
+        "HEPG2": 2,
+        "IMR90": 3,
+        "K562": 4
+    } 
+
+    train_dataset = PeaksEmbeddingsDataset(peaks_h5, elements_tsv, chroms_train, classes, cache_dir='/home/vqj407/workspace/')
+    val_dataset = PeaksEmbeddingsDataset(peaks_h5, elements_tsv, chroms_val, classes, cache_dir='/home/vqj407/workspace/')
+
+    model = CNNEmbeddingsPredictor(input_channels, hidden_channels, kernel_size, out_channels=len(classes))
+    train_peak_classifier(train_dataset, val_dataset, model, num_epochs, out_dir, batch_size, lr, num_workers, prefetch_factor, device, progress_bar=True, resume_from=resume_checkpoint)
+
+    wandb.finish()
