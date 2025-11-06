1938b9ccdeba97520f913afa15096ca84fa1fa53
PanosAntoniadis <pantoniadis97@gmail.com>
2025-11-06 13:35:12 +0100

    Fix bugs

--- src/dnalm_bench/task_2_5_single/experiments/task_3_peak_classification/eval_probing/rnalm.py
+++ src/dnalm_bench/task_2_5_single/experiments/task_3_peak_classification/eval_probing/rnalm.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
 
     modes = {"train": chroms_train, "val": chroms_val, "test": chroms_test}
 
-    input_channels = 256
+    input_channels = 768
     hidden_channels = 32
     kernel_size = 8
 
