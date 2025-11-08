bd99600613336d0117cc192d8ea303167c83b98e
PanosAntoniadis <pantoniadis97@gmail.com>
2025-11-08 10:01:55 +0100

    Update embedding extraction

--- src/dnalm_bench/task_2_5_single/experiments/task_3_peak_classification/extract_embeddings/rnalm.py
+++ src/dnalm_bench/task_2_5_single/experiments/task_3_peak_classification/extract_embeddings/rnalm.py
@@ -8,11 +8,8 @@ from ....components import SimpleSequence
 root_output_dir = os.environ.get("DART_WORK_DIR", "")
 
 if __name__ == "__main__":
-    model_name = "rnalm_144M_H_MLM"
-    output_dir = "/tmp/vqj407/rnalm_erda/gefion_output//outputs/mlm_pretraining/runs/2025-03-26_00-14-15_evenLargerModel/"
-    checkpoint_path = "best"
-    use_metadata = True
-    tokenizer_path = "/home/vqj407/workspace/RNALM/rnalm/tokenizers/dna_tokenizer"
+    model_name = "144M_H_MLM_last"
+    use_track_embeddings = False
     genome_fa = os.path.join(root_output_dir,"refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta")
     elements_tsv = os.path.join(root_output_dir,"task_3_peak_classification/processed_inputs/peaks_by_cell_label_unique_dataloader_format.tsv")
     chroms = None
@@ -28,10 +25,7 @@ if __name__ == "__main__":
         dir="outputs/wandb",
         config={
             "model_name": model_name,
-            "checkpoint_path": checkpoint_path,
-            "output_dir": output_dir,
-            "use_metadata": use_metadata,
-            "tokenizer_path": tokenizer_path,
+            "use_track_embeddings": use_track_embeddings,
             "task": "task_3",
             "approach": "extract_embeddings",
             "batch_size": batch_size,
@@ -42,11 +36,10 @@ if __name__ == "__main__":
         }
     )
 
-    out_path = os.path.join(root_output_dir,f"task_3_peak_classification/embeddings/{model_name}_{use_metadata}.h5")
+    out_path = os.path.join(root_output_dir,f"task_3_peak_classification/embeddings/{model_name}_trackEmbeddings{use_track_embeddings}.h5")
 
     dataset = SimpleSequence(genome_fa, elements_tsv, chroms, seed)
-    extractor = RNALMEmbeddingExtractor(output_dir, checkpoint_path, use_metadata,
-                                        tokenizer_path, batch_size, num_workers, device)
+    extractor = RNALMEmbeddingExtractor(model_name, use_track_embeddings, batch_size, num_workers, device)
     extractor.extract_embeddings(dataset, out_path, progress_bar=True)
 
     wandb.finish()
